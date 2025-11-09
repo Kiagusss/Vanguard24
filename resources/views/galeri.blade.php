515a4d6c6b33d69638237e0c098da657998befda
kiagusss <109852147+Kiagusss@users.noreply.github.com>
2025-11-09 13:22:24 +0700

    change title

--- resources/views/galeri.blade.php
+++ resources/views/galeri.blade.php
@@ -5,7 +5,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>StreamLine - Workflow Automation Made Simple</title>
+  <title>Vanguard - Informatics 24 Sultan Ageng Tirtayasa</title>
+  <link rel="icon" href="{{ asset('images/Vanguard-Logo.jpg') }}" type="image/x-icon">
   <link
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
@@ -13,6 +14,7 @@
   <script
     src="https://unpkg.com/@lottiefiles/dotlottie-wc@0.8.5/dist/dotlottie-wc.js"
     type="module"></script>
+   
 </head>
 
 <body>
